5414429cf237dc832e384111615a380e1cdf4f41
thomas <hello@thomasflorentin.net>
2021-01-20 14:27:46 +0100

    Page template

--- htdocs/wp-content/themes/wp_courses/page.php
+++ htdocs/wp-content/themes/wp_courses/page.php
@@ -1,18 +1,47 @@
 <?php get_header(); ?>
+
+
+
+
 <main id="content">
+
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<div class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</div>
-</article>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
+
+    <aside class="sidebar ">
+
+        <nav class="course_menu">
+
+            <?php wp_link_pages(); ?>
+            
+        </nav>
+    </aside>
+
+
+    <section class="main course_content">
+
+        <header class="course_head">
+            <h1><?php the_title(); ?></h1>
+        </header>
+
+        <div class="">
+            <?php the_content(); ?>
+        </div>
+
+        <footer class="footer">
+            <?php get_template_part( 'nav', 'below-single' ); ?>
+        </footer>
+
+    </section>
+
+
 <?php endwhile; endif; ?>
+
+
 </main>
-<?php get_sidebar(); ?>
+
+
+
+
 <?php get_footer(); ?>
