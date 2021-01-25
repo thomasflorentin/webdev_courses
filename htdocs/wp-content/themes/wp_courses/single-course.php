569c7a8f3e8b78ce7264fc8f91d21716c5b166cc
thomas <hello@thomasflorentin.net>
2021-01-25 09:30:43 +0100

    Add Q type & Polish

--- htdocs/wp-content/themes/wp_courses/single-course.php
+++ htdocs/wp-content/themes/wp_courses/single-course.php
@@ -94,7 +94,7 @@
                             setup_postdata($post); ?>
                             
                             <li class="course_child">
-                                <a href="<?php the_permalink(); ?>" class="course_child_link  <?php if($current_url == get_permalink() ) { echo 'active'; } ?>">
+                                <a href="<?php the_permalink(); ?>" class="course_child_link  <?php if($current_url == get_permalink() ) { echo 'active'; } ?> <?php if( get_field('is_interview') ) { echo 'interview_item'; } ?>">
                                     <?php the_title(); ?>
                                 </a>
                             </li>
