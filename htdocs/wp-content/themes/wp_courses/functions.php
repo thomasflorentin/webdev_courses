82542432ec0fb170591651e7d8a8740001bffa4c
thomas <hello@thomasflorentin.net>
2021-02-05 15:49:12 +0100

    Redirect simple user after logging

--- htdocs/wp-content/themes/wp_courses/functions.php
+++ htdocs/wp-content/themes/wp_courses/functions.php
@@ -195,3 +195,17 @@ function clp_login_head() {
          
 }
 add_action('login_head', 'clp_login_head');
+
+
+
+
+/**
+ * Redirect non-admins to the homepage after logging into the site.
+ *
+ * @since 	1.0
+ */
+function acme_login_redirect( $redirect_to, $request, $user  ) {
+	return ( is_array( $user->roles ) && in_array( 'administrator', $user->roles ) ) ? admin_url() : site_url();
+}
+add_filter( 'login_redirect', 'acme_login_redirect', 10, 3 );
+
